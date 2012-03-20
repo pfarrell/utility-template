2f5def0805f9cb3493c108f5266ef58ae556ae91
Patrick Farrell <mr.patfarrell@gmail.com>
2012-03-20 16:36:04 -0400

    initial import

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Decryptor
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            ConsoleEngineBase engine = new Decryptor();
+            engine.Main(args);
+        }
+    }
+
+    class Decryptor : ConsoleEngineBase
+    {
+        protected override void ProcessLine(string line)
+        {
+            Console.WriteLine(line);
+        }
+    }
+}
